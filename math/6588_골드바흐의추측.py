583a94808ff169e978bd98e426d61316c046fa03
abc980823 <80378041+abc980823@users.noreply.github.com>
2021-07-24 21:43:04 +0900

    Update 6588_골드바흐의추측.py

--- math/6588_골드바흐의추측.py
+++ math/6588_골드바흐의추측.py
@@ -1,25 +1,31 @@
 max = 1000000
 check = [True]*(max+1)
-count = 0
-p_num = []
 check[0] = check[1] = False
+p_num = []
+count = 0
 for i in range(2,max+1):
     if check[i] == True:
         j = i+i
         p_num.append(i)
         count += 1
-        while j <=max:
+        while j <= max:
             check[j] = False
             j += i
-while True:
-    n = int(input())
-    if n == 0 :
+while(True):
+    N = int(input())
+    if N == 0:
         break
-    for i in range(1,count+1):
-        if check[n-p_num[i]] == True:
-            print(n,'=', p_num[i] ,'+', (n - p_num[i]))
+    i = 1
+    while i < count:
+        if check[N-p_num[i]] == True and N-p_num[i]>0 and N-p_num[i] != 2:
+            print(N,'=',p_num[i],'+',N-p_num[i])
             break
+        i += 1
+    if i == count:
+        print("Goldbach's conjecture is wrong.")
 """
 아리스토텔레스의 체 방법으로 소수들을 모두 구해놓은 뒤, 
 입력받은 수 - 소수 를 한 수가 소수이면 출력을 하는 방식을 사용하였다.
+또, 두 홀수 소수의 합으로 n을 나타낼 수 없는 경우에는
+"Goldbach's conjecture is wrong."을 출력한다.
 """
