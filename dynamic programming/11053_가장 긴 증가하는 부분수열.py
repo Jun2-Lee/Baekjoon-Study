2a719782d1f6e7fec958d3aef01a58e5ec8d1024
abc980823 <80378041+abc980823@users.noreply.github.com>
2021-08-12 23:31:27 +0900

    Create 11053_가장 긴 증가하는 부분수열.py

---
+++ dynamic programming/11053_가장 긴 증가하는 부분수열.py
@@ -0,0 +1,13 @@
+N = int(input())
+A = list(map(int,input().split()))
+d = [0]*N
+max_d = 0
+for i in range(N):
+    d[i] = 1                          # 모든 경우의 초기값을 1로 초기화 해 준다.
+    for j in range(0,i):
+        if A[i] > A[j]:               # 수열의 수가 증가하고 있고,
+            if d[i] < d[j]+1:         # 앞의 부분수열 중에서 가장 길게 이어진 부분수열 뒤에 A[i]를 붙인다 
+                d[i] = d[j]+1
+    if max_d < d[i]:
+        max_d = d[i]
+print(max_d)
